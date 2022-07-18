11f2e9c4af2ac80020b8afe376fb1369deae3b89
Oliver Heamon <oliverheamon@gmail.com>
2022-07-18 20:54:31 +0100

    added additional music

--- ClamExtraMusic.cs
+++ ClamExtraMusic.cs
@@ -11,9 +11,12 @@ namespace ClamExtraMusic
             AddContent(new BossMusicSceneEffect("CrimsonCortex", NPCID.BrainofCthulhu));
             AddContent(new BossMusicSceneEffect("CurseOfTheDungeon", NPCID.SkeletronHead));
             AddContent(new BossMusicSceneEffect("EvilEarthquake", NPCID.EaterofWorldsHead));
+            AddContent(new BossMusicSceneEffect("FieryFistsOfStone", NPCID.Golem));
+            AddContent(new BossMusicSceneEffect("Gardenmetal", NPCID.Plantera));
             AddContent(new BossMusicSceneEffect("GelatineThrone", NPCID.KingSlime));
             AddContent(new BossMusicSceneEffect("HellOrHighWaters", NPCID.DukeFishron));
             AddContent(new BossMusicSceneEffect("ItSeesEverything", NPCID.EyeofCthulhu, NPCID.Deerclops));
+            AddContent(new BossMusicSceneEffect("Omnipotence", NPCID.MoonLordCore));
             AddContent(new BossMusicSceneEffect("PlayingWithFirepower", NPCID.TheDestroyer, NPCID.SkeletronPrime, NPCID.Retinazer, NPCID.Spazmatism));
             AddContent(new BossMusicSceneEffect("PurgatorysAssault", NPCID.BloodNautilus));
             AddContent(new BossMusicSceneEffect("StingingOnslaught", NPCID.QueenBee));
